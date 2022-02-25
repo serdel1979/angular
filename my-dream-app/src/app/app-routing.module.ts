3f436503fe2619eb7e29370ae0a8259ec716cb8a
unknown <sdlbsso@gmail.com>
2022-02-25 10:45:38 -0300

    agregado rutas del menú

--- my-dream-app/src/app/app-routing.module.ts
+++ my-dream-app/src/app/app-routing.module.ts
@@ -6,16 +6,16 @@ import { ItemComponent } from './pages/item/item.component';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
 
 const app_routes: Routes = [
-    { path: '', component: PortafolioComponent },
+    { path: 'home', component: PortafolioComponent },
     { path: 'about', component: AboutComponent },
     { path: 'item', component: ItemComponent },
-    { path: '**', pathMatch: 'full', redirectTo: '' }
+    { path: '**', pathMatch: 'full', redirectTo: 'home' }
 
 ];
 
 @NgModule({
     imports:[
-        RouterModule.forRoot(app_routes)
+        RouterModule.forRoot(app_routes, { useHash: true })
     ],
     exports:[
         RouterModule
