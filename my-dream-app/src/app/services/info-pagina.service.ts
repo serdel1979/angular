5de46f91e3343e9271a8798a684b883f92016816
unknown <sdlbsso@gmail.com>
2022-02-25 11:37:26 -0300

    error en el servicio

---
+++ my-dream-app/src/app/services/info-pagina.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoPaginaService {
+
+  constructor( private http: HttpClient) {
+    //se dispara cuando se recarga la página
+    //console.log('Servicio listo')
+    //con este servicio hacer peticiones get, post a servidores rest, etc
+    this.http.get('assets/data/data-pagina.json')
+    .subscribe(resp=>{
+      console.log(resp);
+    })
+   }
+}
