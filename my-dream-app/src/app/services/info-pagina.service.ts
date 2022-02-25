25825cf3b79fa3d20837726a913636b9f9bf1339
unknown <sdlbsso@gmail.com>
2022-02-25 16:18:50 -0300

    servicio funciona ok

--- my-dream-app/src/app/services/info-pagina.service.ts
+++ my-dream-app/src/app/services/info-pagina.service.ts
@@ -8,7 +8,7 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 export class InfoPaginaService {
   constructor( private http: HttpClient) {
     //se dispara cuando se recarga la página
-    console.log('Servicio listo')
+    console.log('Servicio listo!!')
     //con este servicio hacer peticiones get, post a servidores rest, etc
     this.http.get('assets/data/data-pagina.json')
     .subscribe(resp=>{
